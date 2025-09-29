0bcf7c3784ba9b44a64c5478d00d90069380d4db
Stefan Musarra <stefan0@stefan0.com>
2025-09-29 13:07:43 -0700

    restored conditionals

--- lessons/loops.py
+++ lessons/loops.py
@@ -1,7 +1,9 @@
-
-fruits = ["apple", "banana", "cherry"] # [apple, banana, cherry]
+fruits = ["apple", "banana", "cherry"]  # [apple, banana, cherry]
 for i in range(len(fruits)):
     print(fruits[i])
 
 for i, fruit in enumerate(fruits):
     print(fruits[i])
+
+for _ in [1] * 6:
+    print(1)
