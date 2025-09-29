621d8c5a4455bf8c82529956a935136d113c76da
Stefan Musarra <stefan0@stefan0.com>
2025-09-29 12:35:48 -0700

    Revert "conditionals"
    This reverts commit b4e48882a4679e7bc3fdc0e63de59acfd57db8b4.
    
    	deleted:    lessons/conditional.py
    	modified:   lessons/loops.py
    	deleted:    lessons/strings.py
    	modified:   missionbit.code-workspace

--- lessons/loops.py
+++ lessons/loops.py
@@ -1,9 +1,7 @@
-fruits = ["apple", "banana", "cherry"]  # [apple, banana, cherry]
+
+fruits = ["apple", "banana", "cherry"] # [apple, banana, cherry]
 for i in range(len(fruits)):
     print(fruits[i])
 
 for i, fruit in enumerate(fruits):
     print(fruits[i])
-
-for _ in [1] * 6:
-    print(1)
